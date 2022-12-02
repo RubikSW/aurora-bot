6efc3d9b923980dc462c481af4f30aae7033fbb2
Rubik <cdymlr@outlook.com>
2022-12-01 21:29:11 -0500

    improve multi-channel support

--- handlers/messages.js
+++ handlers/messages.js
@@ -7,7 +7,7 @@ module.exports = (client) => {
         client.on('messageCreate', (message) => {
             if (message.author.bot) return false;
             let found = false
-            const channel = client.channels.cache.find(channel => channel.name === "aurora-logs")
+            const channel = message.guild.channels.cache.find(channel => channel.name === "aurora-logs")
             data.forEach((trigger) => {
                 if (found == true) {return false}
                 const clean_message = message.content.toLowerCase().replace(/[^a-z0-9]/gi, '')
@@ -17,11 +17,18 @@ module.exports = (client) => {
                         new ButtonBuilder()
                             .setCustomId('sendinfo_' + message.author.id)
                             .setLabel('Send Help')
+                            .setEmoji("❤️") 
                             .setStyle(ButtonStyle.Success),
+                        new ButtonBuilder()
+                            .setCustomId('report_')
+                            .setLabel('Report Message')
+                            .setEmoji("⚠️") 
+                            .setStyle(ButtonStyle.Danger),
                     );
                     const CriticalActEmbed = new EmbedBuilder()
                         .setTitle("Aurora")
                         .setDescription("Sent by: <@" + message.author.id + ">")
+                        .setFooter({ text: "https://discord.com/channels/" + message.guild.id + "/" + message.guild.id })
                         .setColor("#a4c16a")
                         .setThumbnail("https://m.media-amazon.com/images/I/61IT3BhhUsL._AC_UF894,1000_QL80_.jpg")
                         .addFields(
@@ -37,10 +44,23 @@ module.exports = (client) => {
         client.on(Events.InteractionCreate, async interaction => {
             if (!interaction.isButton()) return;
             if (interaction.customId.includes("sendinfo_")) {
+
+                const CriticalActions = new ActionRowBuilder()
+                .addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('report_')
+                        .setLabel('Report Message')
+                        .setEmoji("⚠️") 
+                        .setStyle(ButtonStyle.Danger),
+                );
+
                 const ply = interaction.customId.substring(9);
-                client.users.send(ply, "Hey <@" + ply + ">, I noticed you were feeling down and your friends are worried about you ❤️ If you're having a difficult time right now, allow me to send you a few link which may help. There is always someone to talk to, plus it's free and completely anonymous! https://findahelpline.com. If you're in the US, simply dial 988!").catch(() => interaction.channel.send("error: user declined direct message from server members. If you believe that they are in danger of suicide and you have their contact information, contact your local law enforcement for immediate help. You can also encourage the person to contact a suicide prevention hotline."));
-                await interaction.update ({content: "I've just sent <@" + ply + "> some helpful resources. If you believe that they may be in danger of suicide and you have their contact information, contact your local law enforcement for immediate help.", components: [], embeds: [] });
+                client.users.send(ply, "Hey! I'm Aurora, a Discord bot. If you're feeling down, remember that there is always someone to talk to. It's free and completely anonymous! https://findahelpline.com and if you're in the US, you can also dial 988 ❤️").catch(() => interaction.channel.send("error: user declined direct message from server members. If you believe that they are in danger of suicide and you have their contact information, contact your local law enforcement for immediate help. You can also encourage the person to contact a suicide prevention hotline."));
+                await interaction.update ({content: "I've just sent <@" + ply + "> some helpful link. If you believe that they may be in danger of suicide and you have their contact information, contact your local law enforcement for immediate help.", components: [CriticalActions]});
             }    
+            if (interaction.customId.includes("report_")) {
+                await interaction.reply ({content: "To report this message to Discord's Trust & Safety team, you will need this message URL → " + interaction.message.embeds[0].footer.text + " ←. Submit your report here: https://support.discord.com/hc/en-us/requests/new?ticket_form_id=360000029731. If you believe that they may be in danger of suicide and you have their contact information, contact your local law enforcement for immediate help.", components: [], ephemeral: true});
+            } 
         });
         
     }catch (e){
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 ﻿const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits, ChannelType, PermissionsBitField } = require('discord.js');
-const { token} = require('./config.json');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType, ChannelType, PermissionsBitField } = require('discord.js');
+const { token, clientId} = require('./config.json');
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 client.commands = new Collection();
 const commandsPath = path.join(__dirname, 'commands');
@@ -16,24 +16,57 @@ for (const file of commandFiles) {
 
 client.once(Events.ClientReady, () => {
 	console.log('Aurora is online!');
+
+	client.user.setPresence({
+		activities: [{ name: `988`, type: ActivityType.Listening }],
+		status: 'online',
+	});
+
 	const id = client.guilds.cache.map(guild => guild.id);
 	id.forEach((server) => {
 		const guild = client.guilds.cache.get(server);
-		const log_channel = client.channels.cache.find(channel => channel.name === "aurora-logs");
+		const log_channel = guild.channels.cache.find(channel => channel.name === "aurora-logs");
 		if (!log_channel) {
 			guild.channels.create({
 				name: "aurora-logs" ,
 				type: ChannelType.GuildText,
+				topic: "Aurora is a passive bot that monitors your server's messages to detect patterns of emotional distress or suicidal ideations. This private channel will be used to notify Server Administrators of any concerning messages. Feel free to move this channel anywhere but do not rename it. To protect the privacy of your users, **keep this channel private and role-locked to server owners & admins only**",
 				permissionOverwrites: [
 					{
 						id: server, //everyone
 						deny: [PermissionsBitField.Flags.ViewChannel],
+					},
+					{
+						id: clientId, //bot
+						allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages],
 					}
 				]
 			})
 		};	
+		console.log(server);
 	});
 });
+
+client.on('guildCreate', (guild) => {
+	const log_channel = guild.channels.cache.find(channel => channel.name === "aurora-logs");
+	if (!log_channel) {
+		guild.channels.create({
+			name: "aurora-logs" ,
+			type: ChannelType.GuildText,
+			topic: "Aurora is a passive bot that monitors your server's messages to detect patterns of emotional distress or suicidal ideations. This private channel will be used to notify Server Administrators of any concerning messages. Feel free to move this channel anywhere but do not rename it. To protect the privacy of your users, **keep this channel private and role-locked to server owners & admins only**",
+			permissionOverwrites: [
+				{
+					id: guild.id, //everyone
+					deny: [PermissionsBitField.Flags.ViewChannel],
+				},
+				{
+					id: clientId, //bot
+					allow: [PermissionsBitField.Flags.ViewChannel, PermissionsBitField.Flags.SendMessages],
+				}
+			]
+		})
+	};
+})
 
 client.on(Events.InteractionCreate, async interaction => {
 	if (!interaction.isChatInputCommand()) return;
